7ba9e99cc7915ef3c93fb9a622d87df46115f35f
Sébastien Piquemal <sebpiq@protonmail.com>
2025-09-26 10:51:37 +0200

    welcome screen redesign

--- app/src/Welcome.tsx
+++ app/src/Welcome.tsx
@@ -17,37 +17,45 @@ const Welcome: React.FC<WelcomeProps> = () => {
   return (
     <div className="welcome-page Page">
       <div className="welcome-container">
-        <h1 className="welcome-title">Introduction à la géopolitique du climat</h1>
-        <p className="welcome-text">
+        <div className="welcome-title">
+          <div className="welcome-title-background"></div>
+          <h1>Introduction<br/>à la géopolitique<br/>du climat</h1>
+          <button className="welcome-button" onClick={() => dispatch(startPlaying())}>
+            Démarrer
+          </button>
+          <div className="welcome-title-scroll-hint">
+            Faites défiler pour en savoir plus &#x2193;
+          </div>
+        </div>
+        <div className="welcome-instructions">
+          <p className="welcome-text">
 Bienvenue dans ce petit jeu pédagogique sur la géopolitique du climat. Dans celui-ci, vous incarnez les dirigeant·e·s d'un pays qui font des choix dans son industrialisation.
 Le jeu se déroule en une série de tours lors desquelles vous aurez le choix entre plusieurs cartes comme celle-ci :
-</p>
+          </p>
 
-<Card card={DECK[0]} status="available" />
+          <Card card={DECK[0]} status="available" />
 
-<p className="welcome-text">
-Chacune a 4 caractéristiques : D'un côté son coût, correspondant à l'argent nécessaire pour pouvoir
-la jouer (en haut à gauche). De l'autre, ce qu'elle rapportera à tous les tours suivants : 
-</p>
+          <p className="welcome-text">
+          Chacune a 4 caractéristiques : D'un côté son coût, correspondant à l'argent nécessaire pour pouvoir
+          la jouer (en haut à gauche). De l'autre, ce qu'elle rapportera à tous les tours suivants : 
+          </p>
 
-<p className="welcome-text">
-un revenu {<IncomeIcon />},
-une valeur de score {<ScoreIcon />} 
-et une valeur de pollution {<PollutionIcon />}.
-</p>
+          <p className="welcome-text">
+          un revenu {<IncomeIcon />},
+          une valeur de score {<ScoreIcon />} 
+          et une valeur de pollution {<PollutionIcon />}.
+          </p>
 
-<p className="welcome-text">
-Au début de chaque tour, vous gagnerez donc de l'argent en fonction de votre revenu et votre score
-augmentera pour chaque carte qui vous en rapporte. Vous piocherez également 2 nouvelles cartes qui
-s'ajouteront à vos options de jeu. Enfin, la pollution globale augmentera d'une valeur égale à la
-somme de la pollution de TOUS les pays.
+          <p className="welcome-text">
+          Au début de chaque tour, vous gagnerez donc de l'argent en fonction de votre revenu et votre score
+          augmentera pour chaque carte qui vous en rapporte. Vous piocherez également 2 nouvelles cartes qui
+          s'ajouteront à vos options de jeu. Enfin, la pollution globale augmentera d'une valeur égale à la
+          somme de la pollution de TOUS les pays.
 
-Lorsque la pollution atteint {MAX_GLOBAL_POLLUTION}, la partie s'arrête. S'affichera alors un tableau des scores, exposant toutes les données de chaque pays.
-Lequel aura "gagné" ? Ce sera à vous de le déterminer...
-        </p>
-        <button className="welcome-button" onClick={() => dispatch(startPlaying())}>
-          DÉMARRER
-        </button>
+          Lorsque la pollution atteint {MAX_GLOBAL_POLLUTION}, la partie s'arrête. S'affichera alors un tableau des scores, exposant toutes les données de chaque pays.
+          Lequel aura "gagné" ? Ce sera à vous de le déterminer...
+          </p>
+        </div>
       </div>
     </div>
   );
